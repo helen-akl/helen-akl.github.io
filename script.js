1f82f2ec3071e6dd11e9ae9c81f0033b969dddb8
Helen Aklilu <haklilu@fwparker.org>
2026-01-14 08:54:14 -0600

    work

--- script.js
+++ script.js
@@ -1,158 +1,147 @@
-let resultsCounter = 0
-
-function getResults() {
-	let deadrats = document.getElementById('deadrats');
-	if(deadrats.checked) { // if filled in
-		console.log("The user chose dead rats");
-		resultsCounter = resultsCounter + 0;
-	}
-
-	let bears = document.getElementById('bears');
-	if(bears.checked) {
-		console.log("The user chose bears");
-		resultsCounter = resultsCounter + 1;
-	}
-
-	let humans = document.getElementById('humans');
-	if(humans.checked) {
-		console.log("The user chose humans");
-		resultsCounter = resultsCounter + 2;
-	}
-
-	let twofaces = document.getElementById('twofaces');
-		if(twofaces.checked) {
-			console.log("The user chose twofaces");
-			resultsCounter = resultsCounter + 0;
-		}
-
-	let hat = document.getElementById('hat');
-		if(hat.checked) {
-			console.log("The user chose hat");
-			resultsCounter = resultsCounter + 1;
-		}
-
-	let animalskin = document.getElementById('animalskin');
-		if(animalskin.checked) {
-			console.log("The user chose animalskin");
-			resultsCounter = resultsCounter + 2;
-		}
-
-	let thumbsup = document.getElementById('thumbsup');
-		if(thumbsup.checked) {
-			console.log("The user chose thumbsup");
-			resultsCounter = resultsCounter + 0;
-		}
-
-	let beetle = document.getElementById('beetle');
-		if(beetle.checked) {
-			console.log("The user chose beetle");
-			resultsCounter = resultsCounter + 1;
-		}
-
-	let humanface = document.getElementById('humanface');
-		if(humanface.checked) {
-			console.log("The user chose humanface");
-			resultsCounter = resultsCounter + 2;
-		}
-
-	let twofigures = document.getElementById('twofigures');
-			if(twofigures.checked) {
-				console.log("The user chose twofigures");
-				resultsCounter = resultsCounter + 0;
-			}
-
-	let racoon = document.getElementById('racoon');
-			if(racoon.checked) {
-				console.log("The user chose racoon");
-				resultsCounter = resultsCounter + 1;
-			}
-
-	let butterfly = document.getElementById('butterfly');
-			if(butterfly.checked) {
-				console.log("The user chose butterfly");
-				resultsCounter = resultsCounter + 2;
-			}
-
-	if(resultsCounter == 0) {
-		document.getElementById("body").style.backgroundColor="#fbf8cc";
-		const soundOne= new Audio("taco-bell-bong-sfx-120135.mp3");
-		soundOne.play();
-		document.getElementById("bigresult").innerText="You are ... TACO BELL!"
-		document.getElementById("imageresult").src = "tacobell.png"
-		document.getElementById("littleresult").innerText="Your answers suggest you are very go-with-the-flow, but you also have a bit of spice!"
-	}
-
-	if(resultsCounter == 1) {
-		document.getElementById("body").style.backgroundColor="#fde4cf";
-		const soundTwo= new Audio("cow-moo-390282.mp3");
-		soundTwo.play();
-		document.getElementById("bigresult").innerText="You are ... CHICK-FILA!"
-		document.getElementById("imageresult").src = "chickfila.png"
-		document.getElementById("littleresult").innerText="Your answers suggest you are a cow who loves chicken! Huh"
-	}
-
-	if(resultsCounter == 2) {
-		document.getElementById("body").style.backgroundColor="#ffcfd2";
-		const soundThree= new Audio("tada-fanfare-a-6313.mp3");
-		soundThree.play();
-		document.getElementById("bigresult").innerText="You are ... McDONALDS!"
-		document.getElementById("imageresult").src = "mcdonalds.png"
-		document.getElementById("littleresult").innerText="Your answers suggest you love the classics, and might be a little boring. Sorry!"
-	}
-
-	if(resultsCounter == 3) {
-		document.getElementById("body").style.backgroundColor="#f1c0e8";
-		const soundThree= new Audio("tada-fanfare-a-6313.mp3");
-		soundThree.play();
-		document.getElementById("bigresult").innerText="You are ... CULVERS !"
-		document.getElementById("imageresult").src = "culvers.jpeg"
-		document.getElementById("littleresult").innerText="Your answers suggest you LOVEE the midwest and have fantastic taste."
-	}
-
-	if(resultsCounter == 4) {
-		document.getElementById("body").style.backgroundColor="#cfbaf0";
-		const soundSix= new Audio("cricket-chirp-101026.mp3");
-		soundSix.play();
-		document.getElementById("bigresult").innerText="You are ... ARBY'S !"
-		document.getElementById("imageresult").src = "arbys.png"
-		document.getElementById("littleresult").innerText="Your answers suggest you are a little strange and clearly compensating for something. Look into self-help."
-	}
-
-	if(resultsCounter == 5) {
-		document.getElementById("body").style.backgroundColor="#a3c4f3";
-		const soundFive= new Audio("yay-6326.mp3");
-		soundFive.play();
-		document.getElementById("bigresult").innerText="You are ... WENDY'S !"
-		document.getElementById("imageresult").src = "wendys.png"
-		document.getElementById("littleresult").innerText="Your answers suggest you are a sassy little ginger girl at heart."
-	}
-
-	if(resultsCounter == 6) {
-		document.getElementById("body").style.backgroundColor="#90dbf4";
-		const soundSix= new Audio("cricket-chirp-101026.mp3");
-		soundSix.play();
-		document.getElementById("bigresult").innerText="You are ... RAISING CANES!"
-		document.getElementById("imageresult").src = "raisingcanes.png"
-		document.getElementById("littleresult").innerText="Your answers suggest you are a huge trend follower who loves bland food."
-	}
-
-	if(resultsCounter == 7) {
-		document.getElementById("body").style.backgroundColor="#8eecf5";
-		const soundSeven= new Audio("sad-trumpet-46384.mp3");
-		soundSeven.play();
-		document.getElementById("bigresult").innerText="You are ... CHIPOTLE!"
-		document.getElementById("imageresult").src = "chipotle.png"
-		document.getElementById("littleresult").innerText="Your answers suggest you are an incredibly nostalgic person who needs to let go of the past. Burritos will never be below $14 again, and deep down, you know that." 
-	}
-
-	if(resultsCounter == 8) {
-		document.getElementById("body").style.backgroundColor="#b9fbc0";
-		const soundSeven= new Audio("sad-trumpet-46384.mp3");
-		soundSeven.play();
-		document.getElementById("bigresult").innerText="You are ... BURGER KING!"
-		document.getElementById("imageresult").src = "burgerking.png"
-		document.getElementById("littleresult").innerText="Your answers suggest you are have a massive ego and no substance. Make sure to think before you claim yourself the King of anything"
-	}
-
-
+let stats= {
+   "Ayla Pressman": {
+      "#": 12,
+      "Grade": "Junior",
+      "mpg": 25.2,
+      "ppg": 1.5,
+      "apg": 1.8,
+      "rpg": 4.2,
+      "photo": "ayla.png"
+   },
+   "Cassie Rank": {
+      "#": 21,
+      "Grade": "Freshman",
+      "mpg": 6.1,
+      "ppg": 0.3,
+      "apg": 0,
+      "rpg": 0.6,
+      "photo": "cassie.jpg"
+   },
+   "Helen Aklilu": {
+      "#": 24,
+      "Grade": "Senior",
+      "mpg": 12.5,
+      "ppg": 1.2,
+      "apg": 0.5,
+      "rpg": 3.7,
+      "photo":"helen.jpg"
+   },
+   "Jojo Husbeck": {
+      "#": 3,
+      "Grade": "Senior",
+      "mpg": 19.5,
+      "ppg": 9.8,
+      "apg": 0.2,
+      "rpg": 6.8,
+      "photo": "jojo.jpg"
+   },
+   "Jade Labkon": {
+      "#": 0,
+      "Grade": "Junior",
+      "mpg": 9.3,
+      "ppg": 0.3,
+      "apg": 0,
+      "rpg": 1.5,
+      "photo": "jade.jpg"
+   },
+   "Khalia King": {
+      "#": 22,
+      "Grade": "Freshman",
+      "mpg": 3,
+      "ppg": 0,
+      "apg": 0,
+      "rpg": 0,
+      "photo": "khalia.jpg"
+   },
+   "Kiley Moss": {
+      "#": 4,
+      "Grade": "Senior",
+      "mpg": 16.8,
+      "ppg": 1.8,
+      "apg": 0.8,
+      "rpg": 2.3,
+      "photo": "kiley.jpg"
+   },
+   "Kate Stauber": {
+      "#": 23,
+      "Grade": "Sophmore",
+      "mpg": 18.7,
+      "ppg": 3.4,
+      "apg": 0.5,
+      "rpg": 1.8,
+      "photo": "kate.jpg"
+   },
+   "Miriam Johnson": {
+      "#": 1,
+      "Grade": "Junior",
+      "mpg": 24.5,
+      "ppg": 5.9,
+      "apg": 0.5,
+      "rpg": 3.5,
+      "photo": "miriam.jpg"
+   },
+   "Mia Vitacek": {
+      "#": 33,
+      "Grade": "Senior",
+      "mpg": 13.9,
+      "ppg": 1.3,
+      "apg": 0.1,
+      "rpg": 4.3,
+      "photo": "mia.jpg"
+   },
+   "Sloane Kins": {
+      "#": 10,
+      "Grade": "Junior",
+      "mpg": 26,
+      "ppg": 8,
+      "apg": 1.1,
+      "rpg": 14.2,
+      "photo": "sloane.jpg"
+   },
+   "Samantha Kovler": {
+      "#": 20,
+      "Grade": "Sophmore",
+      "mpg": 8,
+      "ppg": 0.6,
+      "apg": 0,
+      "rpg": 1.7,
+      "photo": "samantha.jpg"
+   },
+   "Tyler Jones": {
+      "#": 11,
+      "Grade": "Sophmore",
+      "mpg": 5.6,
+      "ppg": 1.3,
+      "apg": 0,
+      "rpg": 0.6,
+      "photo": "tyler.jpg"
+   }
 }
+
+var items = Object.keys(stats)
+var selectElem = document.getElementById("players");
+
+for (var i = 0; i < items.length; i++){
+  var item = items[i];
+  var element = document.createElement("option");
+  element.innerText = item;
+  selectElem.append(element);
+}
+
+var mySelect = document.getElementById('playerselect');
+ mySelect.onchange = (event) => {
+     var playerName = event.target.value;
+     var playerData = stats[playerName]
+     console.log(playerData)
+    document.getElementById("mpg").innerHTML=playerData["mpg"]
+    document.getElementById("ppg").innerHTML=playerData["ppg"]
+    document.getElementById("image").src="images/" + playerData["photo"]
+    document.getElementById("gradennumber").innerHTML= playerData["Grade"] + ", #" + playerData["#"]
+    document.getElementById("name").innerHTML= playerName
+    document.getElementById("apg").innerHTML=playerData["apg"]
+    document.getElementById("rpg").innerHTML=playerData["rpg"]
+
+   
+
+    
+ }
